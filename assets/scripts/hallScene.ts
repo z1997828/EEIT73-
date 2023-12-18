d6f536a0419a51628aa5fdec457a7c826b583e6c
Vic <hasaki@toothpaste.com>
2023-12-18 11:29:08 +0800

    背景音樂按鈕

--- assets/scripts/hallScene.ts
+++ assets/scripts/hallScene.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node,AudioSource,Button,SpriteFrame  } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('hallScene')
@@ -9,19 +9,32 @@ export class hallScene extends Component {
     @property(Node) rule: Node = null;
     @property(Node) setting: Node = null;
     @property(Node) face: Node = null;
+    @property(Button)MusicButton: Button = null;
+    @property(Button)AudioButton: Button = null;
+    @property(SpriteFrame)onImage: SpriteFrame = null;
+    @property(SpriteFrame)offImage: SpriteFrame = null;
+    @property(AudioSource) bgMusic: AudioSource = null;
 
     public openMenu = false;
+    private MusicIsOn: boolean = !false;
+    private AudioIsOn: boolean = !false;
     onLoad() {
+        this.mall.active = false;
         this.record.active = false;
+        this.feedback.active = false;
+        this.rule.active = false;
+        this.setting.active = false;
+        this.face.active = false;
+        this.openMenu = false;
     }
 
     // ----------上方功能列-------------
     // 頭像按鈕
     public onFace() {
         if(!this.openMenu)
-        this.face.active = true,
-        this.openMenu = true;
-    
+        this.face.active = !false,
+        this.openMenu = !false;
+        console.log("確定按鈕被點擊");
     }
 
 
@@ -54,8 +67,8 @@ export class hallScene extends Component {
     // 商城按鈕
     public onMall() {
         if(!this.openMenu)
-        this.mall.active = true,
-        this.openMenu = true;
+        this.mall.active = !false,
+        this.openMenu = !false;
     }
 
     // 商城內儲值金額1按鈕
@@ -92,15 +105,15 @@ export class hallScene extends Component {
     // 戰績按鈕
     public onRecord() {
         if(!this.openMenu)
-        this.record.active = true,
-        this.openMenu = true;
+        this.record.active = !false,
+        this.openMenu = !false;
     }
 
     // 反饋按鈕
     public onFeedback() {
         if(!this.openMenu)
-        this.feedback.active = true,
-        this.openMenu = true;
+        this.feedback.active = !false,
+        this.openMenu = !false;
     }
 
     // 反饋內提交按鈕
@@ -110,17 +123,40 @@ export class hallScene extends Component {
     // 設定按鈕
     public onSetting() {
         if(!this.openMenu)
-        this.setting.active = true,
-        this.openMenu = true;
+        this.setting.active = !false,
+        this.openMenu = !false;
     }
+
 
     // 設定內音樂開關按鈕
     public onMusic() {
+        this.MusicIsOn = !this.MusicIsOn;
+        if (this.MusicIsOn) {
+            this.MusicButton.normalSprite = this.onImage;
+            this.MusicButton.pressedSprite = this.onImage;
+            this.bgMusic.volume = 1;
+            // this.bgMusic.play();
+        } else {
+            this.MusicButton.normalSprite = this.offImage;
+            this.MusicButton.pressedSprite = this.offImage;
+            this.bgMusic.volume = 0;
+            // this.bgMusic.stop();
+        }
         console.log("確定按鈕被點擊");
     }
 
     // 設定內音效開關按鈕
     public onAudio() {
+        this.AudioIsOn = !this.AudioIsOn;
+        if (this.AudioIsOn) {
+            this.AudioButton.normalSprite = this.onImage;
+            this.AudioButton.pressedSprite = this.onImage;
+            // this.bgMusic.volume = 1;
+        } else {
+            this.AudioButton.normalSprite = this.offImage;
+            this.AudioButton.pressedSprite = this.offImage;
+            // this.bgMusic.volume = 0;
+        }
         console.log("確定按鈕被點擊");
     }
 
@@ -131,8 +167,8 @@ export class hallScene extends Component {
     //// 玩法按鈕
     public onRule() {
         if(!this.openMenu)
-        this.rule.active = true,
-        this.openMenu = true;
+        this.rule.active = !false,
+        this.openMenu = !false;
     }
 }
 
