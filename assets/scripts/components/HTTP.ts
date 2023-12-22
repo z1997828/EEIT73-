c51bec8d426b7d58d61f0e145de8366e4205dc23
Andy <z1997828@gmail.com>
2023-12-22 16:05:37 +0800

    1222調整遊戲場景

--- assets/scripts/components/HTTP.ts
+++ assets/scripts/components/HTTP.ts
@@ -4,7 +4,7 @@ export default class HTTP {
     
     // 發送 HTTP 請求的方法
     // 可以傳入路徑（path）、資料（data）、回調函數（handler）、額外的 URL（extraUrl）
-    sendRequset(path?, data?, handler?, extraUrl?) {
+    getRequset(path?, data?, handler?, extraUrl?) {
         // 如果 extraUrl 未提供，則使用預設的 URL
         if (extraUrl == null) {
             extraUrl = this.URL;
