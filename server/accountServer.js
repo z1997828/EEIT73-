d66b29e6319b036b0f258fb53c3789364282feeb
Andy <z1997828@gmail.com>
2024-01-03 00:29:10 +0800

    將後臺網站與Server分離並放入server資料夾內

--- server/accountServer.js
+++ server/accountServer.js
@@ -14,15 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 // Web 伺服器的靜態檔案置於 public 資料夾
-app.use("/public",express.static("../public"));
+app.use("/public", express.static("../public"));
 
 app.all('*', (req, res, next) => {
-    res.header('Access-Control-Origin', "*");
-    res.header('Access-Control-Allow-Headers', "X-Requested-With");
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS,DELETE');
-    res.header("X-Powered-By", '3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
+  res.header('Access-Control-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS,DELETE');
+  res.header("X-Powered-By", '3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
 })
 
 
@@ -30,38 +30,37 @@ app.all('*', (req, res, next) => {
 
 
 app.get('/', (req, res) => {
-    res.send('Hello');
-  });
-
-  app.get('/login', (req, res) => {
-    let account =  req.query.account;
-    let password = req.query.password;
-
-    checkDB.getAccount(account, password, (err, rows) => {
-       
-    });
+  res.send('Hello');
 });
-  
-app.get('/get_serverinfo', (req, res) => {
-    const data = { version: '0.0.1' };
-    console.log(data);
-    res.send(data);
+
+app.get('/login', (req, res) => {
+  let account = req.query.account;
+  let password = req.query.password;
+
+  checkDB.getData(account, password,(err,row)=>{
+    
   });
+});
+app.get('/get_serverinfo', (req, res) => {
+  const data = { version: '0.0.1' };
+  console.log(data);
+  res.send(data);
+});
 
 app.post('/getPost', (req, res) => {
-    console.log(req.body)
-  });
+  console.log(req.body)
+});
 // 一切就緒，開始接受用戶端連線
 
 http.listen(3000);
 sio.on('connection', (socket) => {
-    console.log("客戶端:有人連接到Server")
-    socket.on("game_ping", () => {
-        socket.emit("game_pong")
-    })
-    socket.on("disconnect", () => {
-        console.log("客戶端:有人離開Server")
-    })
+  console.log("客戶端:有人連接到Server")
+  socket.on("game_ping", () => {
+    socket.emit("game_pong")
+  })
+  socket.on("disconnect", () => {
+    console.log("客戶端:有人離開Server")
+  })
 
 })
 console.log("Web伺服器就緒，開始接受用戶端連線.");
