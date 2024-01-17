a66262e513153d0ae5510249cb57ecff88a72aac
Zan <vans914@gmail.com>
2024-01-17 16:46:57 +0800

    更新login.js chat.js package.json

--- function/login.js
+++ function/login.js
@@ -1,9 +1,10 @@
 const bcrypt = require('bcryptjs')
 const admin = require('firebase-admin');
+const express = require('express')
 
 const serviceAccount = require("../gameproject-d9074-firebase-adminsdk-6rnh9-cff9fb8858.json");
 const firebase = require('firebase/app');
-// const getAuth = getAuth(firebaseApp);
+require('firebase/auth');
 const firebaseConfig = {
     apiKey: "AIzaSyCEEb5PlBygA9_pTl38ce19A5vtZsKUqdA",
     authDomain: "gameproject-d9074.firebaseapp.com",
@@ -15,31 +16,36 @@ const firebaseConfig = {
     measurementId: "G-CL35V2SP5F"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const auth = firebaseApp.auth();
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
+const auth = firebaseApp.auth();
 const db = admin.firestore();
-//查詢資料功能
+const jwt = require('jsonwebtoken');
 const firebaseTimestamp = admin.firestore.FieldValue.serverTimestamp();
+const crypto=require('crypto')
+const secretKey = crypto.randomBytes(32).toString('base64');
 
-exports.registerNewUser = function(username, email, password) {
+
+// 註冊功能
+exports.registerNewUser = function (username, email, password) {
     return new Promise((resolve, reject) => {
 
         auth.createUserWithEmailAndPassword(email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
-        
+
                 // 使用bcrypt加密
                 bcrypt.hash(password, 10, (err, hashedPassword) => {
                     if (err) {
                         console.error('密碼加密失敗', err);
                         return reject(err);
                     }
-        
+
                     // 額外將資料寫入db - 在此處寫入資料庫
                     db.collection('users').doc(username).set({
-                        avatar:"",
+                        avatar: "",
                         email: email,
                         username: username,
                         password: hashedPassword, // 存儲加密後的密碼
@@ -52,7 +58,7 @@ exports.registerNewUser = function(username, email, password) {
                             }).catch(function (error) {
                                 console.log('驗證郵件發送失敗', error)
                             });
-        
+
                             console.log('用戶資料寫入成功');
                             resolve(user);
                         })
@@ -60,7 +66,7 @@ exports.registerNewUser = function(username, email, password) {
                             console.error('寫入用戶資料失敗', error);
                             reject(error);
                         });
-        
+
                     console.log('註冊成功', user);
                 });
             })
@@ -114,7 +120,45 @@ exports.getUsers = function authenticateUser(email, inputPassword) {
                         return reject(new Error('密碼比對錯誤'));
                     }
                     if (result) {
-
+                        let feedback = [];
+                        db.collection('feedback').doc(userData.username).get()
+                            .then((snapshot) => {
+                                feedback = snapshot.data().message;
+                            }).catch((err) => {
+                                feedback = '';
+                            })
+                        let user_playway = [];
+                        db.collection('game_playway')
+                            .get()
+                            .then((snapshot) => {
+                                snapshot.forEach((doc) => {
+                                    switch (userData.username) {
+                                        case doc.data().banker_id:
+                                            user_playway.push({
+                                                identity: 'banker',
+                                                money: doc.data().banker_money,
+                                                date: doc.data().date.toDate().toLocaleString()
+                                            })
+                                            break;
+                                        case doc.data().player1_id:
+                                            user_playway.push({
+                                                identity: 'player1',
+                                                money: doc.data().player1_money,
+                                                date: doc.data().date.toDate().toLocaleString()
+                                            })
+                                            break;
+                                        case doc.data().player2_id:
+                                            user_playway.push({
+                                                identity: 'player2',
+                                                money: doc.data().player2_money,
+                                                date: doc.data().date.toDate().toLocaleString()
+                                            })
+                                            break;
+                                    }
+                                });
+                            }).catch((err) => {
+                                console.log(err);
+                            })
                         // 密碼匹配，使用 Firebase Authentication 登錄
                         firebase.auth().signInWithEmailAndPassword(email, inputPassword)
                             .then((userCredential) => {
@@ -123,10 +167,18 @@ exports.getUsers = function authenticateUser(email, inputPassword) {
                                     email: userData.email,
                                     username: userData.username,
                                     money: userData.money,
-                                    regtime: userData.regtime
+                                    regtime: userData.regtime,
+                                    feedback: feedback,
+                                    user_playway:user_playway
                                 };
+                                    // 生成 JWT
+                                    const token = jwt.sign({ username: userDetails.username }, secretKey, { expiresIn: '72h' });
+                                    // 将 JWT 添加到用户详情中
+                                    userDetails.token = token;
+
                                 // 登錄成功
                                 resolve(userDetails);
+
                             })
                             .catch((error) => {
                                 // Firebase 登錄失敗
@@ -146,67 +198,47 @@ exports.getUsers = function authenticateUser(email, inputPassword) {
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 function signInWithGoogle() {
-    return new Promise((resolve,reject)=>{
-    auth.signInWithPopup(googleProvider)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            db.collection('users').doc(user.uid).set({
-                displayName: user.displayName,
-                email: user.email,
-                uid: user.uid
-            })
-                .then(() => {
-                    console.log('使用者資料已存入Firestore');
-                    resolve(user);
+    return new Promise((resolve, reject) => {
+        auth.signInWithPopup(googleProvider)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                db.collection('users').doc(user.uid).set({
+                    displayName: user.displayName,
+                    email: user.email,
+                    uid: user.uid
                 })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    console.error('Google 登入失敗', errorCode, errorMessage);
-                    reject(error);
-                });
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error('Google 登入失敗', errorCode, errorMessage);
-            reject(error);
-        });
+                    .then(() => {
+                        console.log('使用者資料已存入Firestore');
+                        resolve(user);
+                    })
+                    .catch((error) => {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        console.error('Google 登入失敗', errorCode, errorMessage);
+                        reject(error);
+                    });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error('Google 登入失敗', errorCode, errorMessage);
+                reject(error);
+            });
     });
 }
 exports.signInWithGoogle = signInWithGoogle;
 //密碼重設
 function resetPassword(email) {
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         auth.sendPasswordResetEmail(email)
-        .then(()=>{
-            console.log("重設密碼郵件已發送至"+email)
-            resolve();
-        })
-        .catch((error)=>{
-            console.log('重設密碼郵件發送失敗',error.message);
-            reject(error);
-        })
+            .then(() => {
+                console.log("重設密碼郵件已發送至" + email)
+                resolve();
+            })
+            .catch((error) => {
+                console.log('重設密碼郵件發送失敗', error.message);
+                reject(error);
+            })
     })
 }
 exports.resetPassword = resetPassword;
-
-/*
-const { ccclass, property } = cc._decorator;
-
-@ccclass
-export default class YourScriptName extends cc.Component {
-
-// 假設這是按鈕的點擊事件處理函式
-onClickLoginButton() {
-    // 呼叫 Google 登入函式
-    this.signInWithGoogle();
-}
-
-// Google 登入函式，這裡放上你的 signInWithGoogle() 函式的內容
-signInWithGoogle() {
-    // 實作你的 Google 登入邏輯
-    // ...
-}
-}
-*/
