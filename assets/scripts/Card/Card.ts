a95366790f775d5421fb8936f07472ddc08f90c4
Andy <z1997828@gmail.com>
2024-01-30 22:15:21 +0800

    完成房間初始化以及線上玩家可視化

--- assets/scripts/Card/Card.ts
+++ assets/scripts/Card/Card.ts
@@ -1,32 +1,37 @@
-import { _decorator, Component, Node, Sprite, SpriteAtlas } from 'cc';
-const { ccclass, property } = _decorator;
-import { RoomState } from '../components/define';
+import { _decorator, Component, Node, Prefab, Sprite, SpriteAtlas } from 'cc';
 import gameManager from '../components/gameManager';
-@ccclass('card')
+import { RoomState } from '../components/define';
+const { ccclass, property } = _decorator;
+
+@ccclass('Card')
+
 export default class Card extends Component {
-    @property({ type: SpriteAtlas })
+    @property(SpriteAtlas)
     public cardsSpriteAtlas: SpriteAtlas | null = null;
-
-    private flag: boolean = false;
-    private offset_y: number = 20;
-    private cardId: number | undefined;
-    private cardData: any; // 根據實際情況定義類型
-    private username: string | undefined;
-
+    @property(Prefab)
+    public pokerPrefab: Prefab = null;
+    @property(Node)
+    public pokerContainer: Node = null;
+    private flag = false;
+    private offset_y = null;
+    private card_id = null;
+    private card_data = null;
+    private username = '';
+    private CardValue = null;
+    
     onLoad() {
-        this.flag = false
         this.offset_y = 20
-        
-        this.node.on("reset_card_flag",function(event){
-            if(this.flag==true){
+
+        this.node.on("reset_card_flag", function (event) {
+            if (this.flag == true) {
                 this.flag = false
                 this.node.y -= this.offset_y
             }
         }.bind(this))
     }
-    
 
-     
+
+
     runToCenter() {
         //移动到屏幕中间，并带一个牌缩小的效果
     }
@@ -40,67 +45,98 @@ export default class Card extends Component {
     }
     // 其他方法同理轉換，省略...
 
-    // setTouchEvent() {
-    //     if (this.username === gameManager.Instance.userDetails.username) {
-    //         this.node.on(Node.EventType.TOUCH_START, this.onCardTouch, this);
-    //     }
-    // }
+    setTouchEvent() {
+        if (this.username == gameManager.Instance.userDetails.userDetails) {
+            this.node.on(Node.EventType.TOUCH_START, function (event) {
+                var gameroom_node = this.node.parent
+                var room_state = gameroom_node.getComponent("gameroom").roomstate
+                if (room_state == RoomState.ROOM_PLAYING) {
+                    console.log("TOUCH_START id:" + this.card_id)
+                    if (this.flag == false) {
+                        this.flag = true
+                        this.node.position.y += this.offset_y
+                        //通知gameui层选定的牌
+                        var carddata = {
+                            "cardid": this.card_id,
+                            "card_data": this.card_data,
+                        }
+                        gameroom_node.emit("choose_card_event", carddata)
+                    } else {
+                        this.flag = false
+                        this.node.y -= this.offset_y
+                        //通知gameUI取消了那张牌
+                        gameroom_node.emit("unchoose_card_event", this.card_id)
+                    }
+                }
 
-    // onCardTouch() {
-    //     const gameSceneNode = this.node.parent;
-    //     if (gameSceneNode) {
-    //         const roomState = gameSceneNode.getComponent('gameroom').roomState;
-    //         if (roomState === RoomState.ROOM_PLAYING) {
-    //             console.log(`TOUCH_START id: ${this.cardId}`);
-    //             this.flag = !this.flag;
-    //             this.node.position = this.node.position.add3f(0, this.flag ? this.offset_y : -this.offset_y, 0);
-    //             // 通知 gameUI 层选定的牌
-    //             gameSceneNode.emit(this.flag ? 'choose_card_event' : 'unchoose_card_event', {
-    //                 cardId: this.cardId,
-    //                 cardData: this.cardData,
-    //             });
-    //         }
-    //     }
-    // }
+            }.bind(this))
+        }
 
-    showCards(card: any, cardsSpriteAtlas: SpriteAtlas) {
-        this.cardData = card;
-        // this.accountId = accountId;
+    }
+
+    showCards(card, username) {
+        //card.index是服务器生成card给对象设置的一副牌里唯一id
+        this.card_id = card.index
+        //传入参数 card={"value":5,"shape":1,"index":20}
+        this.card_data = card
+        if (username) {
+            this.username = username //标识card属于的玩家
+        }
+
+        // 黑桃：spade
+        // 红桃：heart
+        // 梅花：club
+        // 方片：diamond
+        // const CardShape = {
+        //     "S": 1,
+        //     "H": 2,
+        //     "C": 3,
+        //     "D": 4,
+        // };
+
+        const cardShpae = {
+            "1": 3,
+            "2": 2,
+            "3": 1,
+            "4": 0
+        }
+
+        //this.node.getComponent(cc.Sprite).spriteFrame = 
+        //服务器定义牌的表示
+        // const cardvalue = {
+        //     "A": 12,
+        //     "2": 13,
+        //     "3": 1,
+        //     "4": 2,
+        //     "5": 3,
+        //     "6": 4,
+        //     "7": 5,
+        //     "8": 6,
+        //     "9": 7,
+        //     "10": 8,
+        //     "J": 9,
+        //     "Q": 10,
+        //     "K": 11,
+        // }
+    
+        const Kings = {
+            "14": 54,
+            "15": 53
+        }
 
         let spriteKey = '';
+
         if (card.shape) {
-            const cardValueMap = {
-                "12": 1, "13": 2, "1": 3, "2": 4, "3": 5,
-                "4": 6, "5": 7, "6": 8, "7": 9, "8": 10,
-                "9": 11, "10": 12, "11": 13
-            };
+            spriteKey = 'card_' + (cardShpae[card.shape] * 13 + this.CardValue[card.value]);
 
-            // 黑桃：spade
-            // 红桃：heart
-            // 梅花：club
-            // 方片：diamond
-            const cardShapeMap = {
-                "1": 3,
-                "2": 2,
-                "3": 1,
-                "4": 0
-            };
-            spriteKey = 'card_' + (cardShapeMap[card.shape] * 13 + cardValueMap[card.value]);
-        } else if (card.king !== undefined) {
-            const kingsMap = {
-                "14": 54, "15": 53
-            };
-            spriteKey = 'card_' + kingsMap[card.king];
+        } else {
+            spriteKey = 'card_' + Kings[card.king];
         }
-
-        if (this.cardsSpriteAtlas && spriteKey) {
-            const spriteFrame = this.cardsSpriteAtlas.getSpriteFrame(spriteKey);
-            if (spriteFrame) {
-                const spriteComponent = this.node.getComponent(Sprite);
-                if (spriteComponent) {
-                    spriteComponent.spriteFrame = spriteFrame;
-                }
-            }
-        }
+        
+        // console.log("spriteKey"+spriteKey)
+        this.node.getComponent(Sprite).spriteFrame = this.cardsSpriteAtlas.getSpriteFrame(spriteKey)
+        this.setTouchEvent()
     }
+
+
 }
---
+++ assets/scripts/gameRoomSceneUI.ts
@@ -0,0 +1,23 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('gameRoomSceneUI')
+export class gameRoomSceneUI extends Component {
+    
+    onLoad(){
+
+    }
+    
+    public onReady() {
+
+    }
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
